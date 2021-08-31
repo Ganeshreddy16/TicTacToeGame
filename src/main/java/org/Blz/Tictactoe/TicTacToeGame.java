b86edea06ccb1132e438a53f3cbf038d96116694
Ganesh Reddy <reddyganesh125@gmail.com>
2021-08-31 21:10:56 +0530

    Created:Board

---
+++ src/main/java/org/Blz/Tictactoe/TicTacToeGame.java
@@ -0,0 +1,18 @@
+package org.Blz.Tictactoe;
+import java.util.Scanner;
+public class TicTacToeGame
+{
+       public static char [] board = new char[10];
+       public static void fillingEmpty(){
+           for(int i=0;i<10;i++)
+               board[i]=' ';
+
+       }
+    public static void main( String[] args )
+    {
+
+    }
+
+
+
+}
